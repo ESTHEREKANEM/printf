15f53c77de964514226f89639df9306ead484197
Victor Oluwatobi <akomolafevicto@gmail.com>
2022-10-17 23:41:51 -0700

    betty fixed, and added prototype printf to main.h

--- _printf.c
+++ _printf.c
@@ -2,37 +2,36 @@
 #include "main.h"
 /**
  * _printf - This is the entry point for a printf function clone.
- * 
+ *
  * @format: this is the first parameter passed to the function
  * which mostlikely is a string
- * 
+ *
  * @...: this are other functions that would be saved as args.
- * 
- * Return: the number pf characters printed excluding the end output to strings.
+ *
+ * Return: the number of character printed excluding the end output to string.
 */
 
 int _printf(const char *format, ...)
 {
-    int i = 0;
+	int i = 0;
+	va_list args;
 
-    va_list args;
-    va_start (args, format);
+	va_start(args, format);
 
-    for (i = 0; i != '\0'; i++)
-    {
-        if (format[i] == '%')
-        {
-            i++;
-            if (format[i] == 'c')
-            {
-                va_arg(args, char);
-            }
-            else (format[i] == 's')
-            {
-                va_arg(args, *char);
-            }
-        }
+	for (i = 0; i != '\0'; i++)
+	{
+		if (format[i] == '%')
+		{
+			i++;
 
-    }    
-    return (i);
+			if (format[i] == 'c' || format[i] == 's')
+			{
+				char c = (char) va_arg(args, int);
+				_putchar(c);
+			}
+		}
+		_putchar(format[i]);
+	}
+
+	return (i);
 }
